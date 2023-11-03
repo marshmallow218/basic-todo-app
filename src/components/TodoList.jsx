ee856c222c7166c069064ba2bd71b4db06b64dd2
Kurt Renzo Jalalain <jalalainkurt@gmail.com>
2023-11-03 20:54:54 +0800

    initial commit

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+import {TodoItem} from './TodoItem.jsx'
+
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
+    return (
+        <ul className="list">
+            {todos.length === 0 && "Nothing to do..."}
+            {todos.map(todo => {
+                return (
+                    <TodoItem {...todo} key={todo.id} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+                )
+            })}                                          
+        </ul>
+    )
+}
